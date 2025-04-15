ec34ee390afb6d926aa9acc698759705127067bb
Andre Suhartanto <andresuhartanto28@gmail.com>
2025-04-15 12:41:49 +0800

    first commit with structure

---
+++ Goons RepoSearch/Goons RepoSearch/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Goons RepoSearch
+//
+//  Created by Andre on 2025/4/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Initialize the window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        showRepoList()
+        
+        return true
+    }
+}
+
+// Present RepoList Page
+extension AppDelegate {
+    func showRepoList() {
+        guard let window = window else {
+            print("[❗️] Window is nil")
+            return
+        }
+        
+        let repoListViewController = RepoListViewController()
+
+        window.rootViewController = repoListViewController
+        window.makeKeyAndVisible()
+    }
+}
---
+++ Goons RepoSearch/Goons RepoSearch/Pages/RepoList/View/RepoListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RepoListViewController.swift
+//  Goons RepoSearch
+//
+//  Created by Andre on 2025/4/15.
+//
+
+import UIKit
+
+class RepoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
